9fce8c6aae1499a8e48b517b080500c95e91505d
bryantTheCoder <foobryant99@gmail.com>
2021-11-29 09:11:39 +0800

    Update index.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -35,9 +35,8 @@ module.exports = class extends Generator {
       "Finished generating! Before running, make sure you have the Gulp CLI installed globally, and you do NOT have Gulp itself installed globally."
     );
     this.log(" ");
-    this.log(" ");
     this.log(
-      "Once you have the Gulp CLI installed globally, you can run 'gulp' in this project directory to run Gulp!"
+      "Once you have the Gulp CLI installed globally, run Gulp to start your day :)) !"
     );
     this.log(" ");
   }
